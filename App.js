e88270c65afda87e0c20c7f144392db2d4c7ba90
Aldo Guizado <aldoguizado11@gmail.com>
2023-07-24 12:05:03 -0500

    Subida de archivo Login

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TextInput, Image, TouchableOpacity, View, Alert } from 'react-native';
 
 export default function App() {
+  const mensaje = () => {
+    Alert.alert('Mensaje', 'Bienvenido al sistema!');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground style={styles.container} source={require('./assets/img/fondo.jpg')}>
+
+      <Text style={styles.txtTitulo}>Log In</Text>
+
+      <View style={styles.containerForm}>
+        <View style={styles.containerForms}>
+          <Text style={styles.txtForm}>Correo:</Text>
+          <TextInput style={styles.inpForm}
+            placeholder='Ingrese su correo'
+            placeholderTextColor={'#fff'}
+            textContentType='emailAddress'
+          />
+        </View>
+
+        <View style={styles.containerForms}>
+          <Text style={styles.txtForm}>Contraseña:</Text>
+          <TextInput style={styles.inpForm}
+            placeholder='Ingrese su contraseña'
+            placeholderTextColor={'#fff'}
+            textContentType='newPassword'
+          />
+        </View>
+
+        <View style={styles.containerForms}>
+          <TouchableOpacity style={styles.btn} onPress={mensaje}>
+            <Text style={styles.txtBtn}>Ingresar</Text>
+            <Image
+              source={require('./assets/img/send.png')}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+    </ImageBackground>
   );
 }
 
@@ -14,7 +48,58 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+
+  txtTitulo: {
+    color: '#fff',
+    fontSize: 25,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    marginBottom: 25
+  },
+
+  containerForm: {
+    flexDirection: 'column',
+    gap: 20
+  },
+
+  containerForms: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  txtForm: {
+    color: '#fff',
+    width: '20%',
+    marginRight: 10
+  },
+
+  inpForm: {
+    borderColor: '#04fef5',
+    borderWidth: 1,
+    borderRadius: 7,
+    color: '#fff',
+    height: 36,
+    width: '56%',
+    paddingLeft: 10
+  },
+
+  btn: {
+    backgroundColor: '#04fef5',
+    borderRadius: 7,
+    height: 44,
+    width: '30%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 5
+  },
+
+  txtBtn: {
+    fontSize: 16
+  }
+
 });
